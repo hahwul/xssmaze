7fcc13067b68b292d8219dc60265146d10fa7e60
hahwul <hahwul@gmail.com>
2023-03-28 14:36:09 +0900

    Add UA Pattern

--- src/mazes/header.cr
+++ src/mazes/header.cr
@@ -3,4 +3,9 @@ def load_header
     get "/header/level1/" do |env|
         env.response.headers["referer"]
     end
+
+    Xssmaze.push("header-level2", "/header/level2/","user-agent header")
+    get "/header/level2/" do |env|
+        env.response.headers["user-agent"]
+    end
 end
