6576967832ab5dc16d786101678e93344a1d51c0
hahwul <hahwul@gmail.com>
2023-03-30 22:54:01 +0900

    Add path-based patterns

--- src/mazes/path.cr
+++ src/mazes/path.cr
@@ -1,7 +1,25 @@
 def load_path
-    Xssmaze.push("path-level1", "/path/level3/a","")
+    Xssmaze.push("path-level1", "/path/level1/a","reflected")
     get "/path/level1/:name" do |env|
         name = env.params.url["name"]
+        "Hi #{name}"
+    end
+
+    Xssmaze.push("path-level2", "/path/level2/a","escape to %2f")
+    get "/path/level2/:name" do |env|
+        name = env.params.url["name"].gsub("%2f","")
+        "Hi #{name}"
+    end
+
+    Xssmaze.push("path-level3", "/path/level3/a","escape to %20")
+    get "/path/level3/:name" do |env|
+        name = env.params.url["name"].gsub(" ","").gsub("%20","")
+        "Hi #{name}"
+    end
+
+    Xssmaze.push("path-level4", "/path/level4/a","escape to %2f and %20")
+    get "/path/level4/:name" do |env|
+        name = env.params.url["name"].gsub("%2f","").gsub("%20","")
         "Hi #{name}"
     end
 end
