5573c4b42cf08e3002d299a78935d1fb1fa139e1
hahwul <hahwul@gmail.com>
2023-04-07 18:46:30 +0900

    Add DOM XSS Patterns

--- src/mazes/dom.cr
+++ src/mazes/dom.cr
@@ -1,22 +1,40 @@
 def load_dom
-    Xssmaze.push("dom-level1", "/dom/level1/","dom write")
+    Xssmaze.push("dom-level1", "/dom/level1/","dom write (location.href)")
     get "/dom/level1/" do |env|
         "<script>
             document.write(decodeURI(location.href))
         </script>"
     end
 
-    Xssmaze.push("dom-level2", "/dom/level2/","dom write")
-    get "/dom/level2/" do |env|
+    Xssmaze.push("dom-level2", "/dom/level2/","dom write (location.hash)")
+    get "/dom/level2/" do |_|
         "<script>
             document.write(decodeURI(location.hash))
         </script>"
     end
 
-    Xssmaze.push("dom-level3", "/dom/level3/","redirect")
-    get "/dom/level3/" do |env|
+    Xssmaze.push("dom-level3", "/dom/level3/","redirect (location.hash)")
+    get "/dom/level3/" do |_|
         "<script>
             document.location.href = location.hash.replace('#','')
+        </script>"
+    end
+
+    Xssmaze.push("dom-level4", "/dom/level4/","dom write (query param)")
+    get "/dom/level4/" do |_|
+        "<script>
+            const urlParams = new URL(location.href).searchParams;
+            const query = urlParams.get('query');
+            document.write(query)
+        </script>"
+    end
+
+    Xssmaze.push("dom-level5", "/dom/level5/","dom write (query param)")
+    get "/dom/level5/" do |_|
+        "<script>
+            const urlParams = new URL(location.href).searchParams;
+            const query = urlParams.get('query');
+            eval(query)
         </script>"
     end
 end
