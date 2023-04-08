fdc3a5589ca4c5839b40a1c4aba1ce2307712650
hahwul <hahwul@gmail.com>
2023-04-09 01:48:55 +0900

    Add pattern of redirect

--- src/mazes/redirect.cr
+++ src/mazes/redirect.cr
@@ -3,4 +3,14 @@ def load_redirect
     get "/redirect/level1/" do |env|
         env.redirect env.params.query["query"]
     end
+    
+    Xssmaze.push("redirect-level2", "/redirect/level2/","query param")
+    get "/redirect/level2/" do |env|
+        env.redirect env.params.query["query"].gsub("javascript","")
+    end
+
+    Xssmaze.push("redirect-level2", "/redirect/level3/","query param")
+    get "/redirect/level3/" do |env|
+        env.redirect env.params.query["query"].downcase.gsub("javascript","")
+    end
 end
